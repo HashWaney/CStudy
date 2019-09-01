5fe730cfe56060a5bf2058406a10674bf8aebca5
Hash <HashWaney@gmail.com>
2019-09-01 17:33:53 +0800

    update:

---
+++ C++/collection/Container.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Hash on 2019-08-22.
+// Copyright (c) 2019 @https://github.com/HashWaney All rights reserved.
+//
+#include "vector"
+#include "queue"
+#include "string.h"
+#include "iostream"
+#include "stack"
+using  namespace std;
+
+int main()
+{
+    //实例化
+    vector<int> vector1;//默认初始化  vector1为空
+    vector<int> vector2(vector1);//使用vector1初始化vector2
+
+
+
+    
+}
